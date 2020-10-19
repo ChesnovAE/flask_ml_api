f599f89c9c5307d3d2060e20e26287987dd4463d
ChesnovAE <aechesnov@yandex.ru>
2020-10-20 01:20:23 +0300

    ADD: simple model in ml_api/models.py

--- app/ml_api/models.py
+++ app/ml_api/models.py
@@ -1,9 +1,14 @@
 from abc import ABC, abstractclassmethod
+from sklearn.linear_model import LogisticRegression
+import numpy as np
+import pandas as pd
+from typing import Union, Dict
 
 
 class BaseModel(ABC):
-    def __init__(self, params):
+    def __init__(self, params, model=None):
         self.params = params
+        self.model = model(self.params['model_params'])
     
     @abstractclassmethod
     def fit(self, xtrain, ytrain):
@@ -14,9 +19,17 @@ class BaseModel(ABC):
         pass
 
 
-class MyCustomModel(BaseModel):
-    def fit(self, xtrain, ytrain):
-        raise NotImplementedError
+class LogisticModel(BaseModel):
+    def __init__(self,
+                 params: Dict,
+                 model=LogisticRegression) -> None:
+        super().__init__(params, model)
     
-    def predict(self, xtest):
-        raise NotImplementedError
+    def fit(self,
+            xtrain: Union[pd.DataFrame, np.array],
+            ytrain: Union[pd.DataFrame, np.array]) -> None:
+        self.model.fit(xtrain, ytrain)
+    
+    def predict(self,
+                xtest: Union[pd.DataFrame, np.array]) -> np.array:
+        return self.model.predict(xtest)
