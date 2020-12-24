e28bd2f82319c6d579060a920831d1034ff0948e
Ramil <rchermanteev@bk.ru>
2020-12-25 01:19:55 +0300

    update config.py

--- config.py
+++ config.py
@@ -3,16 +3,18 @@ import os
 
 class BaseConfig:
     base_dir = os.path.abspath(os.path.dirname(__file__))
-
     CSRF_ENABLE = True
     DEBUG = True
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
-    ABS_UPLOAD_FOLDER = os.path.join(base_dir, 'app/static/uploads')
-    REL_UPLOAD_FOLDER = 'static/uploads'
-    # STATICFILES_DIRS = (os.path.join(base_dir, "static"))
+    ABS_UPLOAD_FOLDER = os.path.join(base_dir, 'app/static/images/upload_img/')
+    REL_UPLOAD_FOLDER = 'static/images/upload_img/'
+    ABS_STYLIZED_FOLDER = os.path.join(base_dir, 'app/static/images/stylized_img/')
+    REL_STYLIZED_FOLDER = 'static/images/stylized_img/'
+    SEND_FILE_MAX_AGE_DEFAULT = 0
 
 
 class ProdConfig(BaseConfig):
     DEBUG = False
+
 
 print(BaseConfig.base_dir)
